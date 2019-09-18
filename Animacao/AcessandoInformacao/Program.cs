c0e4824843132c1c22686336278575d9a71992d9
F34R1337 <filipeeduardoregis2017@gmail.com>
2019-09-18 16:27:41 -0300

    Commit aula quarta love

---
+++ Animacao/AcessandoInformacao/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MinhaBiblioteca;
+
+namespace AcessandoInformacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuSistema();
+        }
+
+        private static void MenuSistema()
+        {
+            Console.WriteLine("Escolha uma das opções do menu:");
+            Console.WriteLine("1 - Calculo de área");
+            Console.WriteLine("2 - Mostrar Animação");
+            Console.WriteLine("3 - Sair do sistema");
+
+            var menuEscolhido = int.Parse(Console.ReadLine());
+
+            switch (menuEscolhido)
+            {
+                case 1:
+                    {
+                        CalculaArea();
+                        MenuSistema();
+                    }
+                    break;
+                case 2:
+                    {
+                        AnimacoesEmFrames.ExecutarAnimacao();
+                        MenuSistema();
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.WriteLine("Saindo...");
+                        return;
+                    }
+                default:
+                    break;
+            }
+        }
+
+        public static void CalculaArea()
+        {
+            Console.WriteLine("Informa o lado do quadrado:");
+            var ladoQuadrado = int.Parse(Console.ReadLine());
+
+            var bibliotecaCalculos = new CalculosDeArea();
+
+            Console.WriteLine($"Minha área é {bibliotecaCalculos.CalculaAreaDoQuadrado(ladoQuadrado)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Animacao/MinhaBiblioteca/CalculosDeArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaBiblioteca
+{
+    public class CalculosDeArea
+    {
+        /// <summary>
+        /// Metodo para retornar a área de um quadrado
+        /// </summary>
+        /// <param name="ladoDoQuadrado">Lado do quadrado com a precisão inteira</param>
+        /// <returns>Retorna a área total do quadrado</returns>
+        public double CalculaAreaDoQuadrado(int ladoDoQuadrado)
+        {
+            return ladoDoQuadrado* ladoDoQuadrado;
+        }
+    }
+}
