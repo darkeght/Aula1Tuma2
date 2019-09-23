6d27e195a0d0bb99461635f1f3254de6525e5932
F34R1337 <filipeeduardoregis2017@gmail.com>
2019-09-23 14:42:09 -0300

    Commit

--- OrdenandoEFiltrandoLista/CestaDeFrutas/Program.cs
+++ OrdenandoEFiltrandoLista/CestaDeFrutas/Program.cs
@@ -10,8 +10,10 @@ namespace CestaDeFrutas
     {
         static void Main(string[] args)
         {
+            #region Codando muito
             var cestaDeFrutas = new List<Fruta>();
 
+            #region Carregando minha cesta
             //Tomate
             cestaDeFrutas.Add(new Fruta()
             {
@@ -36,23 +38,29 @@ namespace CestaDeFrutas
                 Cor = "Amarelo",
                 Peso = 325
             });
+            #endregion
 
+            #region Lista 1
             cestaDeFrutas.
                 //Neste ponto ordenamos os valores de forma decrescente pelo nome
                 OrderByDescending(x => x.Nome).ToList<Fruta>().
                 ForEach(i => 
                 Console.WriteLine($"Id {i.Id} Nome: {i.Nome}"));
+            #endregion
 
             Console.WriteLine("-----------------------------");
 
+            #region Lista 2
             cestaDeFrutas.
                 //Aqui ordenamos os valores pelo id de forma crescente 
                 OrderBy(x => x.Id).ToList<Fruta>().
                 ForEach(i =>
                 Console.WriteLine($"Id {i.Id} Nome: {i.Nome} Peso: {i.Peso}"));
+            #endregion
 
             Console.WriteLine("----------------------------- Filtro Peso");
 
+            #region Lista 3
             var filtroCesta = cestaDeFrutas
                 //Aqui filtramos os registro maiores de 100 gramas
                 .Where(x => x.Peso > 100)
@@ -68,8 +76,16 @@ namespace CestaDeFrutas
                               select frutinha)//<- Neste ponto eu junto as informações que filtrei e retorno
                               .ToList<Fruta>()//<- Convertemos para uma lista amiga novamente 
                               .ForEach(i => Console.WriteLine($"Fruta escolhida {i.Nome}"));//<- imprimimos estas informações pelo console 
+            #endregion
+
+            Console.WriteLine("----------------------------- Filtro Cores");
+
+            var mostrandoFind = cestaDeFrutas.Find(x => x.Cor == "Amarela" || x.Cor == "Vermelha");
+
+            var mostrandoFindAll = cestaDeFrutas.FindAll(x => x.Cor == "Amarela" || x.Cor == "Vermelha");
 
             Console.ReadKey();
+            #endregion
         }
     }
 }
