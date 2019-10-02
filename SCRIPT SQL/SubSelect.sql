1e7203ff568cd1d257d8b55d51c9229ad10c8aeb
franz <tmppmrv2@outlook.com>
2019-10-02 16:58:33 -0300

    Sub Select

---
+++ SCRIPT SQL/SubSelect.sql
@@ -0,0 +1,8 @@
+﻿select  * from Clientes;
+select  * from Pedidos;
+
+select * from Pedidos ped  -- nossa tabela de quetemos o registros
+where ped.ClienteId in (select cli.Id from Clientes cli -- sub slect em nossa base de dados
+		 inner join Pedidos ped on cli.Id = ped.ClienteId -- Validados 
+where ped.Numero in ('N000000001','N000000006','N000000013')) -- selecionando os valores pelos pedisos
+order by ped.Valor desc -- ordenando os valores
