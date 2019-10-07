7d2a1fcba03448c1354f247aec77c33c56c22d0d
franz <tmppmrv2@outlook.com>
2019-10-07 08:40:43 -0300

    Commit sql exercicios extra numero 1

---
+++ SCRIPT SQL/Exercicio 1 extra.sql
@@ -0,0 +1,22 @@
+﻿INSERT INTO Flores (Nome,Quantidade) VALUES 
+('Rosa',3),
+('Magarida',4),
+('Copo de leite',2),
+('11 horas',5),
+('Violeta',4)
+-- 1.3 - Devemos ter uma seleção ordenada das flores, da que contem a maior quantidade para a menor
+SELECT -- comando de seleção do banco de dados 
+*  -- com * falamos que vamos retornar todos os campos de nosso select
+FROM -- From indica a tabela que vamos selecionar para a contagem de linhas
+Flores -- Nossa primeira tabela de seleção ela representa a quantidade de 
+-- linhas que o select irá realizar
+ORDER BY -- aqui colocamos o comando de ordenação de dados o by indica que ele
+-- espera um campo de nossa tabela selecionada para realizar a ordenação
+Quantidade -- aqui especifiquei o campo que vamos ordernar 
+DESC -- Aqui informo que vamos ordernar do maior para o menos 
+-- por padrão a ordenação já é realizada do menor para o maior
+-- 	1.3 - Devemos retornar uma seleção com a quantidade total de flores que temos em nossa tabela
+SELECT 
+COUNT(*) -- aqui contamos a quantidade de registro dentro do nosso select
+-- não discriminando um campo especificamente 
+FROM Flores -- nossa tabela de dados 
---
+++ SCRIPT SQL/dbo.Playlists.data.sql
@@ -0,0 +1,27 @@
+﻿/*
+'CRIA ASA PERIQUITA',	  'BIRULEIBY',        
+'TCHUTCHUCA TREME TREME', 'MC DELANO',		  
+'NA 4x4',				  'MR CATRA',		  
+'ALO SEGURANÇA',		  'BANDA PASSARELA',  
+'QUERIDA AMIGA',	      'BRILHA SOM',       
+'VOU PARA SANTA CATARINA','TERCEIRA DIMENSAO',
+'CHEVETAO',				  'GAROTOS DE OURO',  
+'FUNDO DA GROTA',		  'BAITACA',          
+'GRITOS DE LIBERDADE',    'GRUPO RODEIO',     
+'ELE TE TRAI',			  'SAN MARINO',       
+'FOTO 3X4'    ,				  'SAN MARINO',      */ 
+
+INSERT INTO Generos VALUES ('FUNK'),('BANDINHA'),('GAUCHAO')
+SELECT * FROM Generos
+INSERT INTO Artistas (GeneroId,Nome) values (12,'BIRULEIBY')
+INSERT INTO Artistas (GeneroId,Nome) values (12,'MC DELANO')
+INSERT INTO Artistas (GeneroId,Nome) values (13,'BANDA PASSARELA')
+INSERT INTO Artistas (GeneroId,Nome) values (13,'SAN MARINO')
+INSERT INTO Artistas (GeneroId,Nome) values (14,'BAITACA')
+SELECT * FROM Artistas
+INSERT INTO Cancoes (ArtistaId,Nome) values (1,'CRIA ASA PERIQUITA')
+INSERT INTO Cancoes (ArtistaId,Nome) values (2,'TCHUTCHUCA TREME TREME')
+INSERT INTO Cancoes (ArtistaId,Nome) values (3,'ALO SEGURANÇA')
+INSERT INTO Cancoes (ArtistaId,Nome) values (4,'ELE TE TRAI')
+INSERT INTO Cancoes (ArtistaId,Nome) values (4,'FOTO 3X4')
+INSERT INTO Cancoes (ArtistaId,Nome) values (5,'FUNDO DA GROTA')
