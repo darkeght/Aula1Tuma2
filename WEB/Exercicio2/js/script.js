91a1607dd42be30335d45004e220581bcc9a3372
darkeght <darkeght@gmail.com>
2019-10-17 17:37:51 -0300

    WQeb

--- WEB/Exercicio2/js/script.js
+++ WEB/Exercicio2/js/script.js
@@ -2,17 +2,35 @@
 $(document).ready(
     function () {
         //Informamos que no botão do click ele ira chamar nosso alerta
-        $('input[name="btnboasvindas"]').click(function () {
-            /* Aqui obtemos o valor digitado pelo usuario */
-            var nomeusuario = $('input[name="nomeusuario"]').val();
-            /* Apresentamos nosso alerta de boas vinda */
-            alert("Seja bem vindo " + nomeusuario + " ao nosso sistema de cervejaria.");
+        $('input[name="bntconverter"]').click(function () {
+
+            var valordolar = 4.16;
+            var valoreal = $('input[name="valoreal"]').val()
+                .replace(".", "")
+                .replace(",", ".");
+            
+
+            var convercao = (valoreal / valordolar)
+                .toFixed(2)
+                .toString()
+                .replace(/(\d)(\d{2})$/, "$1,$2");
+
+            $('input[name="valordolar"]').val(convercao);
+        });
+
+        //Colocamos a mask em nosso campo 999.999,00
+        $('input[name="valoreal"]').mask("000.000,00");
+
+        //usando api externa
+        $.getJSON("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2710-17-2019%27&$top=100&$format=json", function (data) {
+            var teste = data.value[0].cotacaoCompr;
+            //data[0].valoreconvertido analisar o codigo pelo debug para verificar os valores
         });
     }
 );
 
 $(document).on('keypress', function (e) {
     if (e.which === 13) {
-        $('input[name="btnboasvindas"]').click();
+        $('input[name="bntconverter"]').click();
     }
 });
