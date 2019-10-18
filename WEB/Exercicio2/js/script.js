def7a9f0f5f1c05748140459167c1cd3755ceea9
darkeght <darkeght@gmail.com>
2019-10-18 10:46:36 -0300

    Commit

--- WEB/Exercicio2/js/script.js
+++ WEB/Exercicio2/js/script.js
@@ -1,13 +1,17 @@
 /* Ao carregar nosso site por completo executa o conteudo do $(document).ready()*/
 $(document).ready(
     function () {
+        /*Valor padrão ao carregar o site*/
+        $('input[name="valoreal"]').val("9.999,99");
+
         //Informamos que no botão do click ele ira chamar nosso alerta
         $('input[name="bntconverter"]').click(function () {
-
+            /* Nossa url que obtemos os valores da cotação da moeda */
             var url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2710-16-2019%27&$top=100&$format=json";
 
+            /* aqui nesta oarte temos a função getJSON que obtem de uma api web as informações das cotações*/
             $.getJSON(url, function (data) {
-
+                /* obtemos da nossa api web o valor de venda da moeda  */
                 var valordolar = data.value[0].cotacaoVenda;
                 /*  Obter um valor */
                 var valoreal = $('input[name="valoreal"]').val()
@@ -15,8 +19,9 @@ $(document).ready(
                     .replace(".", "")
                     .replace(",", ".");
 
-
+                /* aqui realizamos a converçao da moeda para real */
                 var convercao = (valoreal / valordolar)
+                    /* aqui temos o formato da moeda em um padrão norte americano */
                     .toLocaleString('en-US', { minimumFractionDigits: 2, currency: 'USD' });
 
                 /* Inserir um valor*/
@@ -25,8 +30,7 @@ $(document).ready(
 
         });
 
-        //Colocamos a mask em nosso campo 999.999,00
-        //$('input[name="valoreal"]').mask("000.000,00");
+        //Aqui definimos uma mascara 
         $('input[name="valoreal"]').maskMoney({
             showSymbol: false,
             symbol: "R$",
@@ -35,9 +39,10 @@ $(document).ready(
         });
     }
 );
-
+/* Aqui adicionamos ao on de nosso documento o evento keypress para ao pressionar enter o mesmo chamar o evento */
 $(document).on('keypress', function (e) {
     if (e.which === 13) {
+        /* Com isso temo nosso evento clicl ele desta maneira chama o nosso botão click */
         $('input[name="bntconverter"]').click();
     }
 });
