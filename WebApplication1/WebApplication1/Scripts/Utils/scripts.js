467ca4128d816e666a20bfd95ad6d16f63a51a53
darkeght <darkeght@gmail.com>
2019-10-22 10:51:35 -0300

    Solução web

---
+++ WebApplication1/WebApplication1/Scripts/Utils/scripts.js
@@ -0,0 +1,27 @@
+﻿//carregamento do documento
+$(document).ready(function () {
+    //adicionando uma função ao click do botão
+    $('#Button1').click(function () {
+        //conectando com api web
+        $.getJSON("https://viacep.com.br/ws/RS/Porto%20Alegre/all/json/", function (data) {
+            //loop para cada objeto
+            $.each(data, function (key, value) {
+                //criamos uma variavel que irá armazenar a informação
+                var templateContent = "";
+                //iniciamos um separador de informação
+                templateContent += "{";
+                //outro laço para repetir para cada informação
+                $.each(value, function (key, value) {
+                    //adicionamos todas as informações do nosso objeto
+                    templateContent += key + ":" + value + ",";
+                });
+                //finalizamos nosso objeto
+                templateContent += "},";
+                //adicionamos ao nosso campo de texto
+                $('#divContent').append(
+                    "<textarea>" + templateContent + "</textarea>"
+                );
+            });
+        });
+    });
+});
