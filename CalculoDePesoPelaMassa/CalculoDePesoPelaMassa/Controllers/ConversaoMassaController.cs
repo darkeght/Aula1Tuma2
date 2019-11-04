9bd5ee004145865296585769df3871b2b8d7f2b7
darkeght <darkeght@gmail.com>
2019-11-04 17:57:45 -0200

    Commit

---
+++ CalculoDePesoPelaMassa/CalculoDePesoPelaMassa/Controllers/ConversaoMassaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculoDePesoPelaMassa.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculoDePesoPelaMassa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversaoMassaController : ControllerBase
+    {
+        [HttpPost]
+        public ConversaoMassa Post(ConversaoMassa item)
+        {
+            return item;
+        }
+    }
+}
---
+++ CalculoDePesoPelaMassa/CalculoDePesoPelaMassa/Models/ConversaoMassa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculoDePesoPelaMassa.Models
+{
+    public class ConversaoMassa
+    {
+        public double Densidade { get; set; }
+
+        public double Volume { get;set; }
+
+        public double Gravidade { get; set; } = 9.8;
+
+        public double Peso { get { return Densidade * Volume * Gravidade; } }
+    }
+}
